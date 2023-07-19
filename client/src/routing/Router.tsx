abfb88b82691c6afa8317206334eeb1acc07538a
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-07-19 21:51:55 +0700

    update router

--- client/src/routing/Router.tsx
+++ client/src/routing/Router.tsx
@@ -1,10 +1,17 @@
-import { Navigate, createBrowserRouter } from 'react-router-dom'
 import DefaultLayout from 'layout/Default/DefaultLayout'
 import FullWidthLayout from 'layout/FullWidth/FullWidthLayout'
-import Home from 'pages/Home/Home'
-import Error from 'pages/Error/Error'
+import CourseCancellation from 'pages/CourseCancellation/CourseCancellation'
 import CourseRegistration from 'pages/CourseRegistration/CourseRegistration'
+import EducationProgram from 'pages/EducationProgram/EducationProgram'
+import Error from 'pages/Error/Error'
+import ExamSchedule from 'pages/ExamSchedule/ExamSchedule'
+import GraduationLookup from 'pages/GraduationLookup/GraduationLookup'
+import Home from 'pages/Home/Home'
+import Schedule from 'pages/Schedule/Schedule'
+import Survey from 'pages/Survey/Survey'
 import Transcript from 'pages/Transcript/Transcript'
+import Tuition from 'pages/Tuition/Tuition'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
@@ -19,12 +26,44 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: 'education-program',
+        element: <EducationProgram />,
+      },
+      {
         path: 'course-registration',
         element: <CourseRegistration />,
       },
       {
+        path: 'course-cancellation',
+        element: <CourseCancellation />,
+      },
+      {
+        path: 'schedule',
+        element: <Schedule />,
+      },
+      {
+        path: 'exam-schedule',
+        element: <ExamSchedule />,
+      },
+      {
         path: 'transcript',
         element: <Transcript />,
+      },
+      {
+        path: 'tuition',
+        element: <Tuition />,
+      },
+      {
+        path: 'tuition-payment',
+        element: <Tuition />,
+      },
+      {
+        path: 'survey',
+        element: <Survey />,
+      },
+      {
+        path: 'graduation-lookup',
+        element: <GraduationLookup />,
       },
     ],
   },
