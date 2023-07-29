85363ebba82856b1988c234cfb2a0817bb1adf35
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-07-29 09:26:36 +0700

    Handle register subject

--- client/src/components/CourseRegistration/CourseCheckBox.tsx
+++ client/src/components/CourseRegistration/CourseCheckBox.tsx
@@ -1,4 +1,5 @@
-import { Tooltip } from '@mui/material'
+import { CircularProgress, Tooltip } from '@mui/material'
+import Typography from '@mui/material/Typography'
 import React, { FC, useState } from 'react'
 import { useAppDispatch } from 'redux/store'
 import { registerCourse } from 'redux/store/features/subjectSlice'
@@ -13,9 +14,17 @@ const CourseCheckBox: FC<CourseCheckBoxProps> = props => {
   const dispatch = useAppDispatch()
   const { course } = props
   const [checked, setChecked] = useState<boolean>(course.is_dk)
+  const [loading, setLoading] = useState<boolean>(false)
 
   const register = async (id_to_hoc: string, is_checked: boolean) => {
-    const response = await dispatch(registerCourse({ id_to_hoc, is_checked }))
+    const response = await dispatch(
+      registerCourse({
+        filter: {
+          id_to_hoc,
+          is_checked,
+        },
+      }),
+    )
 
     const registrationResult: CourseRegistrationResponse = response.payload.data
 
@@ -32,15 +41,24 @@ const CourseCheckBox: FC<CourseCheckBoxProps> = props => {
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(e.target.checked)
-    register(course.id_to_hoc, e.target.checked).then(() => {})
+    setLoading(true)
+    register(course.id_to_hoc, e.target.checked).then(() => {
+      setLoading(false)
+    })
   }
 
   return (
     course && (
       <Tooltip title={course.gc_enable} placement="top">
-        <span>
-          <Checkbox checked={checked} disabled={!course.enable} onChange={onChange} />
-        </span>
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <CircularProgress size={20} />
+          </div>
+        ) : (
+          <Typography>
+            <Checkbox checked={checked} disabled={!course.enable} onChange={onChange} />
+          </Typography>
+        )}
       </Tooltip>
     )
   )
