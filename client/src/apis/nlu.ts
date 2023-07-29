85195bf80a866b443bc560fa470925b95fcc433a
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-07-29 15:08:46 +0700

    fix proxy timeout

--- client/src/apis/nlu.ts
+++ client/src/apis/nlu.ts
@@ -11,9 +11,6 @@ const axiosInstance = axios.create({
 })
 
 axiosInstance.interceptors.request.use(async function (config) {
-  config.timeout = 60 * 60 * 1000
-  config.proxy = false
-
   const persistStorage = await storage.getItem('persist:root')
   const { auth } = JSON.parse(persistStorage || '{}')
   const { user, isLogin } = JSON.parse(auth || '{}')
--- server/src/app.ts
+++ server/src/app.ts
@@ -13,12 +13,16 @@ const app = express()
 /*---------------------------------*/
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+)
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }))
 /*---------------------------------*/
-
-app.options('*', cors())
 
 /*---------- API ------------*/
 app.use(function (request, response, next) {
